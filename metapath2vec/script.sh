9eb57585df5464326c44a7ad7378268ad73a3488
Piyush Jain <piyushjain1sa@gmail.com>
2018-11-15 20:15:55 +0530

    Remove baseline method directories

--- metapath2vec/script.sh
+++
@@ -1,2 +0,0 @@
-./metapath2vec -train ../in_new/metapath.apc.w1000.l100.txt -output ../out_new/metapath.apc.w1000.l100 -pp 0 -size 100 -window 7 -negative 5 -threads 44
-./metapath2vec -train ../in_new/metapath.apa.w1000.l100.txt -output ../out_new/metapath.apa.w1000.l100 -pp 0 -size 100 -window 7 -negative 5 -threads 44
